a305451c83abc9e42bb58972b46ca4df9f712914
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2017-12-25 08:00:52 -0500

    integration texting

---
+++ spec/features/running_program_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require './lib/bitmap_editor'
+
+describe 'executing command file' do
+  context 'happy scenario with all possible commands' do
+    it do
+      expect(STDOUT).to receive(:puts) do |bitmap|
+        expect(bitmap.to_s).to eql "OOOOO\nOOZZZ\nAWOOO\nOWOOO\nOWOOO\nOWOOO"
+      end
+
+      BitmapEditor.new.run './spec/fixtures/input1.txt';
+    end
+  end
+
+  context 'with bitmap create command twice' do
+    it do
+      expect(STDOUT).to receive(:puts) do |bitmap|
+        expect(bitmap.to_s).to eql "OOO\nOOZ\nOOO"
+      end
+
+      BitmapEditor.new.run './spec/fixtures/double-create.txt';
+    end
+  end
+
+  context 'showing bitmap with no bitmap' do
+    it do
+      expect(STDOUT).to_not receive(:puts)
+      BitmapEditor.new.run './spec/fixtures/no-bitmap.txt';
+    end
+  end
+end
